73f10b1895daab59c4f869ff0cc5fdc40fdd89bf
Santiago Beguería <santiago.begueria@csic.es>
2023-11-03 20:51:06 +0100

    firstFrost
    Complete overhaul of the function, with more clear code. Not using zoo. Using match() to determine first occurrence of frost. Corrected a bug when type='doy' not giving the correct day within the year, but the day within the seaason (number of days since iniday).

--- R/firstFrost.R
+++ R/firstFrost.R
@@ -1,17 +1,28 @@
 #' Mean first frost day
 #'
-#' @description Calculates the first frost day within a predefined period.
-#' @param mn vector of daily minimum temperature series.
-#' @param dates vector of dates corresponding with daily temprature series
-#' @param iniday first day of the year ("dd-mm") when frost occurrence will be considered.
-#' @param endday last day of the year ("dd-mm") when frost occurrence will be considered.
-#' @param type type of output. It can be "doy" for day of the year (julian day) or "date" for data format ("dd-mm").
-#' @param thres temperature threshold considered to trigger frost occurrence (0 by default).
-#' @details The function is able to span over years. If the argument "enddate" is a date earlier than "inidate", the considered period will cover from "inidate" to the end of the year and from the beginning of the next year to "enddate".
-#' @return Depending on argument type, the output will be a numeric vector of julian days (type = "doy") or a vector of characters with dates (type = "date").
+#' @description Calculates the first frost day (tmp < 0ºC) of the growing
+#' season. Other thresholds can be used.
+#' 
+#' @param mn vector of daily minimum temperature.
+#' @param dates vector of dates corresponding to the mn vector, in Date format.
+#' @param iniday first day of the growing season, in text format ("mm-dd").
+#' Defaults to `07-01`.
+#' @param endday last day of the growing season, in text format ("mm-dd").
+#' Defaults to `06-30`.
+#' @param type character vector specifying the type of output desired. It can
+#' be `doy` for day of the year (julian day), or `date` for character date 
+#' format (`mm-dd`). Defaults to `doy`.
+#' @param thres temperature threshold to trigger frost (defaults to 0ºC).
+#' 
+#' @details The function is able to span over consecutive years. If
+#' `enddate` is an earlier day than `inidate`, then the considered period will
+#' leap over two years.
+#' 
+#' @return Depending on `type`, the output will be a numeric vector of julian
+#' days (of `type` = 'doy') or a vector of characters representing dates
+#' (`type` = 'date').
 #'
 #' @examples
-#'
 #'
 #' firstFrost(mn = daily_tmin,
 #'             dates = seq.Date(as.Date('1981-01-01'),
@@ -19,33 +30,36 @@
 #'             iniday = '07-01',
 #'             endday = '06-30',
 #'             type = 'doy')
-#' @import zoo
+#'
+#' @importFrom lubridate yday
 #' @export
 
 
 firstFrost <- function(mn, dates, iniday = '07-01', endday = '06-30', type = 'doy', thres = 0) {
-      
-      x <- zoo(mn, dates)
-      wini <- which(as.character(time(x), format = '%m-%d') == iniday)
-      wend <- which(as.character(time(x), format = '%m-%d') == endday)
-      if(wend[1] < wini[1]){
-        wend <- wend[2:length(wend)]
-      }
-      z <- rep(NA,length(wini))
-      for (i in 1:(length(wini))) {
-        st <- wini[i]
-        if(i == length(wini)){
-          en <- length(x)
-        } else {
-            en <- wend[i]
-          }
-        f <- which(as.numeric(x)[st:en] < thres)
-        if(length(f) == 0) next
-        if(type == 'doy'){
-          z[i] <- min(f)
-        } else if(type == 'date'){
-          z[i] <- substr(index(x[st:en])[min(f)], 6, 10)
-        }
-      }
-  return(z)
+
+  # determine growing seasons
+  season <- data.frame(
+    ini = which(as.character(dates, format = '%m-%d') == iniday),
+    end = which(as.character(dates, format = '%m-%d') == endday)
+  )
+  if(season$end[1] < season$ini[1]) {
+    season$end <- c(season$end[2:length(season$end)], length(mn))
+  }
+
+  # determine first frost of each growing season
+  ff <- apply(season, 1, function(x) {
+    t <- x['ini']:x['end']
+    w <- match(TRUE, mn[t] < thres)
+    return(dates[t][w])
+  })
+  ff <- as.Date(ff, origin='1970-01-01')
+  
+  # format and return
+  if (type == 'date') {
+    ff <- as.character(ff, format='%m-%d')
+  }
+  if (type == 'doy'){
+    ff <- yday(ff)
+  }
+  return(ff)
 }

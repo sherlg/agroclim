80afa619e68146d3c9c84e184cc55aa7f4b81da7
sherlg <sherrera@eead.csic.es>
2025-05-22 10:30:30 +0200

    update comments test-hs

--- tests/test-hs.R
+++ tests/test-hs.R
@@ -5,7 +5,7 @@ source("R/hs.R")
 #'
 # Test for the calculation of the length of the all periods with guaranteed frost days over years
 test_that("Test for all frost periods calculation", {
-  # Test data: temperatures, dates, and specific day
+  # Test data: temperatures mx and mn, and dates
   set.seed(123) 
   dates <- seq(as.Date("2018-10-01"), as.Date("2022-03-31"), by = "day")
   mn <- round(runif(length(dates), min = -6, max = 2), 1)
@@ -17,6 +17,6 @@ test_that("Test for all frost periods calculation", {
   # Expected result
   expected_result_hs <- c(2,2,2,2,2,2,2,2,2,2)
   
-  # Check if the result matches the expected result for the frost probability
+  # Check if the result matches the expected result for the length of the frost periods
   expect_equal(result_hs, expected_result_hs)
 })

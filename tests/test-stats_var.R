9a1a19dc549d8e358cd9aa06344eb312c1b1b6a8
sherlg <sherrera@eead.csic.es>
2025-07-22 15:28:01 +0200

    Update tests to reflect renamed climate variable parameter.

--- tests/test-stats_var.R
+++ tests/test-stats_var.R
@@ -15,22 +15,22 @@ test_that("Test for 'stats_var' function using default season dates", {
 
   # Default parameters start_day = "07-01", end_day = "06-30"
   # Test: mean
-  result_mean <- stats_var(var = data, dates = dates, operator = "mean") 
+  result_mean <- stats_var(any = data, dates = dates, operator = "mean") 
   expected_result <- c(3.99, 3.88, 3.84)
   expect_equal(result_mean, expected_result, tolerance = 0.1, info = "Mean calculation failed")
   
   # Test: sum
-  result_sum <- stats_var(var = data, dates = dates, operator = "sum")
+  result_sum <- stats_var(any = data, dates = dates, operator = "sum")
   expected_result <- c(1452.4, 1416.6, 1051.0)
   expect_equal(result_sum, expected_result, tolerance = 0.1, info = "Sum calculation failed")
   
   # Test: min
-  result_min <- stats_var(var = data, dates = dates, operator = "min")
+  result_min <- stats_var(any = data, dates = dates, operator = "min")
   expected_result <- c(-2, -2, -2)
   expect_equal(result_min, expected_result, tolerance = 0.1, info = "Min calculation failed")
   
   # Test: max
-  result_max <- stats_var(var = data, dates = dates, operator = "max")
+  result_max <- stats_var(any = data, dates = dates, operator = "max")
   expected_result <- c(10.0, 9.9, 10.0)
   expect_equal(result_max, expected_result, tolerance = 0.1, info = "Max calculation failed")
 })
@@ -44,22 +44,22 @@ test_that("Test for 'stats_var' function especifying period", {
   
   # Default parameters start_day = "01-01", end_day = "04-30"
   # Test: mean
-  result_mean <- stats_var(var = data, dates = dates, operator = "mean", start_day = "01-01", end_day = "04-30") 
+  result_mean <- stats_var(any = data, dates = dates, operator = "mean", start_day = "01-01", end_day = "04-30") 
   expected_result <- c(4.01, 4.03, 4.21, 3.80)
   expect_equal(result_mean, expected_result, tolerance = 0.1, info = "Mean calculation failed")
   
   # Test: sum
-  result_sum <- stats_var(var = data, dates = dates, operator = "sum", start_day = "01-01", end_day = "04-30")
+  result_sum <- stats_var(any = data, dates = dates, operator = "sum", start_day = "01-01", end_day = "04-30")
   expected_result <- c(481.7, 488.1, 504.9, 342.2)
   expect_equal(result_sum, expected_result, tolerance = 0.1, info = "Sum calculation failed")
   
   # Test: min
-  result_min <- stats_var(var = data, dates = dates, operator = "min", start_day = "01-01", end_day = "04-30")
+  result_min <- stats_var(any = data, dates = dates, operator = "min", start_day = "01-01", end_day = "04-30")
   expected_result <- c(-1.9, -2.0, -2.0, -2.0)
   expect_equal(result_min, expected_result, tolerance = 0.1, info = "Min calculation failed")
   
   # Test: max
-  result_max <- stats_var(var = data, dates = dates, operator = "max", start_day = "01-01", end_day = "04-30")
+  result_max <- stats_var(any = data, dates = dates, operator = "max", start_day = "01-01", end_day = "04-30")
   expected_result <- c(9.8,  9.9,  9.9, 10.0)
   expect_equal(result_max, expected_result, tolerance = 0.1, info = "Max calculation failed")
 })

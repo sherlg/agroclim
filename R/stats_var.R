07561f33de4364cfa6c8e69f87aca633fe2d664b
sherlg <sherrera@eead.csic.es>
2025-07-22 15:27:19 +0200

    Rename climate variable parameter to match agroclimGrid function. Update documentation to reflect changes.

--- R/stats_var.R
+++ R/stats_var.R
@@ -1,27 +1,32 @@
 #' Calculate seasonal summary statistics (max, min, mean, sum) of a numeric climate variable.
 #'
-#' @param var Numeric vector representing the variable to be analyzed (e.g., temperature, precipitation).
-#' @param dates Vector of class `Date` corresponding to `var`.
+#' @param any Numeric vector representing the variable to be analyzed (e.g., temperature, precipitation).
+#' @param dates Vector of class `Date` corresponding to `any`.
 #' @param start_day Character string in "mm-dd" format indicating the start of the season. Optional; default is "07-01".
 #' @param end_day Character string in "mm-dd" format indicating the end of the season. Optional; default is "06-30".
 #' @param operator Character string indicating the operation to apply per season. Valid options are `"max"`, `"min"`, `"mean"` and `"sum"`. Optional; default is "mean".
 #'
 #' @return Numeric vector with the result of the chosen operator per season.
 #'
+#' @details 
+#' Only complete seasons are included in the analysis, except for the last one. If the first season does not contain 
+#' all required days based on `start_day` and `end_day`, it is excluded. The last season is included even if incomplete, with analysis 
+#' running up to the last available date.
+#' 
 #' @examples
-#' stats_var(var = temp_data, dates = date_seq, operator = "mean")
+#' stats_var(any = temp_data, dates = date_seq, operator = "mean")
 #'
 #' @import zoo
 
-stats_var <- function(var, dates, start_day = "07-01", end_day = "06-30", operator = "mean") {
+stats_var <- function(any, dates, start_day = "07-01", end_day = "06-30", operator = "mean") {
   # Validations
-  if (length(var) != length(dates)) stop("temp and dates must have the same length.")
+  if (length(any) != length(dates)) stop("temp and dates must have the same length.")
   if (!operator %in% c("max", "min", "mean", "sum")) {
     stop('operator must be one of: "max", "min", "mean", "sum".')
   }
   
   # Create zoo object
-  var_data <- zoo(var, order.by = dates)
+  var_data <- zoo(any, order.by = dates)
   
   # Find indices for start and end of seasons
   start_idx <- which(format(time(var_data), "%m-%d") == start_day)
@@ -36,16 +41,17 @@ stats_var <- function(var, dates, start_day = "07-01", end_day = "06-30", operat
     end_idx <- end_idx[-1]
   }
   
+  # If the last period does not cover all the data, the last data point is used as the end.
+  if(length(end_idx) < length(start_idx)){
+    end_idx <- c(end_idx, length(dates))
+  }
+  
   results <- numeric(length(start_idx))
   
   for(i in seq_along(start_idx)) {
+    # Start and end index for the current period
     st <- start_idx[i]
-    # For last season, go to last available day
-    if(i == length(start_idx)) {
-      en <- length(var_data)
-    } else {
-      en <- end_idx[i]
-    }
+    en <- end_idx[i]
     
     period_data <- var_data[st:en]
     
